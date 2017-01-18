70e2587dae646d02b89468d699b1fc4f33b0b29e
Otto Vehvilainen <otto@mumakil.fi>
2017-01-18 20:28:11 +0200

    Rename verification token

--- lib/slack/configuration.rb
+++ lib/slack/configuration.rb
@@ -5,12 +5,14 @@ module Slack
   # Slack configuration
   class Configuration
 
-    class_attribute :verification_code
+    class_attribute :command
+    class_attribute :command_token
     class_attribute :team_domain
     class_attribute :webhook_url
 
     def self.from_environment!
-      self.verification_code = ENV['SLACK_VERIFICATION_CODE']
+      self.command = ENV['SLACK_COMMAND']
+      self.command_token = ENV['SLACK_COMMAND_TOKEN']
       self.team_domain = ENV['SLACK_TEAM_DOMAIN']
       self.webhook_url = ENV['SLACK_WEBHOOK_URL']
     end
