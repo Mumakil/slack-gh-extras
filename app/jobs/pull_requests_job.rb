ce00120a12316334d24129cf423479d679f48da2
Otto Vehvilainen <otto@mumakil.fi>
2017-01-23 11:50:32 +0200

    Working pulls command

--- app/jobs/pull_requests_job.rb
+++ app/jobs/pull_requests_job.rb
@@ -5,27 +5,31 @@ require 'slack-notifier'
 
 ##
 # Notifies a channel when defaults are changed
-class PullRequesteJob < ApplicationJob
+class PullRequestsJob < ApplicationJob
 
   include ActionView::Helpers::DateHelper
 
   def perform(repositories, original_query, user_id, slack_url)
-    user = nil
-    with_connection_pool do
-      user = User.find(user_id)
+    user = with_connection_pool do
+      User.find(user_id)
     end
     operation = fetch_repo_operation(user.github_token, repositories)
     pull_requests = operation.pull_requests
-    title = "Here are the open pull requests in `#{original_query}`"
+    title = "*All open pull requests (#{pull_requests.size} in total)* in " \
+            "`#{original_query.join(' ')}`"
     pulls = format_pull_requests(pull_requests)
     error = if operation.failed_repositories?
-              text = 'However, there was an error fetching these repositories' \
-                      "    - `#{operation.failed.join('\n    - ')}`"
-              { text: text }
+              text = "However, there was an error fetching these repositories:\n" \
+                      "    - `#{operation.failed_repositories.join("`\n    - `")}`"
+              {
+                text: text,
+                color: 'danger',
+                mrkdwn_in: ['text']
+              }
             end
     slack_notifier(slack_url).post(
-      text: title,
-      attachments: [pulls, error].reject(&:nil?),
+      text: title + "\n\n" + pulls,
+      attachments: [error].reject(&:nil?),
       response_type: 'in_channel'
     )
   rescue RuntimeError => e
@@ -41,20 +45,18 @@ class PullRequesteJob < ApplicationJob
 
   def format_pull_requests(pull_requests)
     if pull_requests.empty?
-      { text: 'There were no open pull requests.' }
+      'There are no open pull requests.'
     else
-      {
-        text: pull_requests.map { |pr| format_pull_request(pr) }.join('\n'),
-        mrkdwn_in: ['text']
-      }
+      pull_requests.map { |pr| format_pull_request(pr) }.join("\n")
     end
   end
 
   def format_pull_request(pull_request)
     repo = pull_request.base.repo
     formatted = "<#{pull_request.url}|#{repo.full_name}##{pull_request.number}>"
-    formatted << " *#{pull_request.title}*"
-    formatted << " _by #{pull_request.assignee.login}_"
-    formatted << ", opened #{time_ago_in_words(Date.parse(pull_request.created_at))} ago"
+    formatted += " *#{pull_request.title}*"
+    formatted += " _by #{pull_request.user.login}_"
+    formatted += ", opened #{time_ago_in_words(pull_request.created_at)} ago"
+    formatted
   end
 end
--- app/models/github/operations/fetch_pull_requests.rb
+++ app/models/github/operations/fetch_pull_requests.rb
@@ -7,32 +7,32 @@ module Github
     # Fetches pull requests for a repo
     class FetchPullRequests < Github::Operation
 
-      attr_reader :pull_requests, :names, :repositories, :failed_repositories
+      attr_reader :pull_requests, :names, :failed_repositories
 
       def initialize(token, names)
         super(token)
         @names = names
-        @repositories = []
+        @pull_requests = []
         @failed_repositories = []
       end
 
       def execute!
-        repositories.each do |repository|
+        names.each do |repository|
           fetch_one(repository)
         end
         self
       end
 
       def pull_requests
-        @pull_requests.sort_by(&:created_at)
+        @pull_requests.flatten.sort_by(&:created_at)
       end
 
       def failed_repositories?
-        !failed_repositories.empty?
+        !@failed_repositories.empty?
       end
 
       def fetch_one(repository_name)
-        @repositories << client.pull_requests(repository_name, state: 'open')
+        @pull_requests << client.pull_requests(repository_name, state: 'open')
       rescue Octokit::ClientError
         @failed_repositories << repository_name
       end
